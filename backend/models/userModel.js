231eb1321d71673a285fb2cf2dbfb324c32c93c3
BC <94580108+BC1337@users.noreply.github.com>
2023-03-13 12:26:12 -0500

    added backend logic for user validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 const bcrpyt = require('bcryptjs')
+const validator = require('validator')
+
+
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
@@ -17,6 +20,17 @@ const userSchema = new Schema({
 // static signup method
 userSchema.statics.signup = async function(email, password) {
 
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email is not a valid email')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password is not strong enough')
+  }
+
   const exists = await this.findOne({ email })
 
   if (exists) {
@@ -27,6 +41,28 @@ userSchema.statics.signup = async function(email, password) {
   const hash = await bcrpyt.hash(password, salt)
 
   const user = await this.create({ email, password: hash })
+
+  return user
+}
+
+// static login method
+
+userSchema.statics.login = async function(email, password) {
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({ email })
+
+  if (!user) {
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrpyt.compare(password, user.password)
+
+  if (!match) {
+    throw Error('Incorrect password')
+  }
 
   return user
 }
